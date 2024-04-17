290980c97690bdf77c1c0b3deead311f911dbe8b
sakibahmedshanto <sakibsanto57@gmail.com>
2024-04-17 22:58:51 +0600

    rafids ui

---
+++ src/main/java/org/example/cinehub/MovieTicketBookingManagement.java
@@ -0,0 +1,26 @@
+package org.example.cinehub;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class MovieTicketBookingManagement extends Application {
+    private double x=0;
+    private double y=0;
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root =FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
